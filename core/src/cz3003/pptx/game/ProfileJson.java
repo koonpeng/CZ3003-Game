4a8e61a738647196f5a6d27f086bb95dbd4b7105
KP <C130185@e.ntu.edu.sg>
2015-04-02 15:39:01 +0800

    Added json library

--- core/src/cz3003/pptx/game/ProfileJson.java
+++ core/src/cz3003/pptx/game/ProfileJson.java
@@ -3,9 +3,16 @@ package cz3003.pptx.game;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.FileNotFoundException;
 import java.util.Iterator;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import com.badlogic.gdx.Gdx;
 
 public class ProfileJson {
 
@@ -22,97 +29,114 @@ public class ProfileJson {
 	private boolean stageFourUnlocked;
 	private boolean stageFiveUnlocked;
 	private boolean stageSixUnlocked;
-	
-	//complete object with all the profiles
+
+	// complete object with all the profiles
 	private JSONObject jsonObj;
 	private boolean dirtyBit;
-	
-	//Constructor
-	public ProfileJson(String username){
+
+	// Constructor
+	public ProfileJson(String username) {
 		this.username = username;
 		this.id = -1;
 		this.difficulty = -1;
 	}
-	
-	//Setter
-	public void setUsername(String username){
-		this.dirtyBit = bool;
+
+	// Setter
+	public void setUsername(String username) {
+		this.dirtyBit = true;
 		this.username = username;
 	}
-	public void setID(int id){
-		this.dirtyBit = bool;
+
+	public void setID(int id) {
+		this.dirtyBit = true;
 		this.id = id;
 	}
-	public void setDifficulty(int difficulty){
+
+	public void setDifficulty(int difficulty) {
 		this.dirtyBit = true;
 		this.difficulty = difficulty;
 	}
-	public void setStageOneUnlocked(boolean bool){
+
+	public void setStageOneUnlocked(boolean bool) {
 		this.dirtyBit = true;
 		this.stageOneUnlocked = bool;
 	}
-	public void setStageTwoUnlocked(boolean bool){
+
+	public void setStageTwoUnlocked(boolean bool) {
 		this.dirtyBit = true;
 		this.stageTwoUnlocked = bool;
 	}
-	public void setStageThreeUnlocked(boolean bool){
+
+	public void setStageThreeUnlocked(boolean bool) {
 		this.dirtyBit = true;
 		this.stageThreeUnlocked = bool;
 	}
-	public void setStageFourUnlocked(boolean bool){
+
+	public void setStageFourUnlocked(boolean bool) {
 		this.dirtyBit = true;
 		this.stageFourUnlocked = bool;
 	}
-	public void setStageFiveUnlocked(boolean bool){
+
+	public void setStageFiveUnlocked(boolean bool) {
 		this.dirtyBit = true;
 		this.stageFiveUnlocked = bool;
 	}
-	public void setStageSixUnlocked(boolean bool){
+
+	public void setStageSixUnlocked(boolean bool) {
 		this.dirtyBit = true;
 		this.stageSixUnlocked = bool;
 	}
 
-	
-	//Getter
-	public String getUsername(){
+	// Getter
+	public String getUsername() {
 		return this.username;
 	}
-	public int getUserID(){
+
+	public int getUserID() {
 		return this.id;
 	}
-	public int getDifficulty(){
-		return difficulty;	
+
+	public int getDifficulty() {
+		return difficulty;
 	}
-	public boolean hasProfile(){
+
+	public boolean hasProfile() {
 		return this.hasProfile;
 	}
-	public boolean isStageOneUnlocked(){
+
+	public boolean isStageOneUnlocked() {
 		return this.stageOneUnlocked;
 	}
-	public boolean isStageTwoUnlocked(){
+
+	public boolean isStageTwoUnlocked() {
 		return this.stageTwoUnlocked;
 	}
-	public boolean isStageThreeUnlocked(){
+
+	public boolean isStageThreeUnlocked() {
 		return this.stageThreeUnlocked;
 	}
-	public boolean isStageFourUnlocked(){
+
+	public boolean isStageFourUnlocked() {
 		return this.stageFourUnlocked;
 	}
-	public boolean isStageFiveUnlocked(){
+
+	public boolean isStageFiveUnlocked() {
 		return this.stageFiveUnlocked;
 	}
-	public boolean isStageSixUnlocked(){
+
+	public boolean isStageSixUnlocked() {
 		return this.stageSixUnlocked;
 	}
-	public boolean getDirtyBit(){
+
+	public boolean getDirtyBit() {
 		return this.dirtyBit;
 	}
-	
-	//Create json file
-	private void createTestJsonFile(){
-	
+
+	// Create json file
+	private void createTestJsonFile() {
+
 		JSONObject obj = new JSONObject();
-		
+
 		try {
 			obj.put("username", "paul");
 			obj.put("id", "100");
@@ -120,41 +144,41 @@ public class ProfileJson {
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
-		
+
 		writeJSONFile(obj);
 	}
-	
-	//overwrite json file with new data
-	public void writeJSONFile(JSONObject obj){
-		
+
+	// overwrite json file with new data
+	public void writeJSONFile(JSONObject obj) {
+
 		try {
-			 
+
 			FileWriter file = new FileWriter(this.JSONFilePath);
 			file.write(obj.toString());
 			file.flush();
 			file.close();
-	 
+
 		} catch (IOException e) {
 			Gdx.app.log(TAG, e.getMessage());
 		}
 	}
 
-	//Retrieve player profile from JSON file
-	public void retrievePlayerProfile() throws IOException{
+	// Retrieve player profile from JSON file
+	public void retrievePlayerProfile() throws IOException {
 		BufferedReader br = null;
 		JSONTokener tokener = null;
 		JSONObject obj = null;
 		String jsonString;
-		
-		try{
+
+		try {
 			br = new BufferedReader(new FileReader(this.JSONFilePath));
 			jsonString = br.readLine();
 			tokener = new JSONTokener(jsonString);
-			
+
 			this.jsonObj = new JSONObject(tokener);
 			obj = this.jsonObj.getJSONObject(this.username);
-			
-			//populate the data
+
+			// populate the data
 			this.id = (int) obj.get("id");
 			this.difficulty = (int) obj.get("difficulty");
 			this.stageOneUnlocked = (boolean) obj.get(stageOneUnlocked);
@@ -163,46 +187,46 @@ public class ProfileJson {
 			this.stageFourUnlocked = (boolean) obj.get(stageFourUnlocked);
 			this.stageFiveUnlocked = (boolean) obj.get(stageFiveUnlocked);
 			this.stageSixUnlocked = (boolean) obj.get(stageSixUnlocked);
-			
+
 			br.close();
-			
-		}catch (FileNotFoundException e){
+
+		} catch (FileNotFoundException e) {
 			Gdx.app.log(TAG, e.getMessage());
-		}catch (JSONException e){
+		} catch (JSONException e) {
 			Gdx.app.log(TAG, e.getMessage());
-		}catch (IOException e){
+		} catch (IOException e) {
 			Gdx.app.log(TAG, e.getMessage());
-		}catch (Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		//if no exception is raised, then we have a valid user profile
+
+		// if no exception is raised, then we have a valid user profile
 		this.hasProfile = true;
 	}
-	
-	//Add / Update new player profile in JSON
-	public void updateJsonObject(){\
-		
+
+	// Add / Update new player profile in JSON
+	public void updateJsonObject() {
+
 		JSONObject obj;
-	
-		if (this.getDirtyBit()){
-			this.jsonObj.remove(this.getUsername);
+
+		if (this.getDirtyBit()) {
+			this.jsonObj.remove(this.getUsername());
 			obj = generateProfileObject();
 			try {
-				this.jsonObj.put(this.getUsername, obj);
+				this.jsonObj.put(this.getUsername(), obj);
 			} catch (JSONException e) {
 				e.printStackTrace();
 			}
-			
+
 			writeJSONFile(this.jsonObj);
-		
-		}else{
-			Gdx.app.log(TAG, "nothing to update")
+
+		} else {
+			Gdx.app.log(TAG, "nothing to update");
 			return;
 		}
 	}
-	
-	public JSONObject generateProfileObject(){
+
+	public JSONObject generateProfileObject() {
 		JSONObject obj = new JSONObject();
 		try {
 			obj.put("username", this.username);
@@ -217,9 +241,8 @@ public class ProfileJson {
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
-		
+
 		return obj;
-		
+
 	}
 }
-
